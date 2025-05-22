aeb0bca9205c6d58490059c19591f0448956cc1c
Евгений <54275499+Dugers@users.noreply.github.com>
2025-05-23 03:36:16 +0700

    add di container with repositories and session maker

---
+++ reservation_bot/di.py
@@ -0,0 +1,24 @@
+from dependency_injector import containers, providers
+
+from reservation_bot.db.connection import get_session
+from reservation_bot.repositories.table.sql_model import TableRepositorySQLModel
+from reservation_bot.repositories.reservation.sql_model import ReservationRepositorySQLModel
+
+class Container(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(modules=[
+        "reservation_bot.services"
+    ])
+
+    session_provider = providers.Resource(get_session)
+
+    table_repository = providers.Factory(
+        TableRepositorySQLModel,
+        session=session_provider
+    )
+
+    reservation_repository = providers.Factory(
+        ReservationRepositorySQLModel,
+        session=session_provider
+    )
+
+container = Container()
